0bbd789d4cfcca3d54c163a509bafcd2a53d76ac
John Wass <wassj@ctc.com>
2015-02-03 19:12:20 -0500

    initial import; reading and writing uncompressed pbo;

---
+++ pbos/src/main/java/com/ctc/vht/pbos/Header.java
@@ -0,0 +1,182 @@
+package com.ctc.vht.pbos;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Collection;
+import java.util.List;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
+import com.google.common.io.ByteProcessor;
+import com.google.common.io.ByteSink;
+
+/**
+ * Models a single PBO header entry
+ * @author wassj
+ *
+ */
+@SuppressWarnings("unused" /* there are several unused fields in the spec */)
+class Header {
+	/**
+	 * the filename \0 + 20; the 20 bytes are packaging, original size, reserved, timestamp, file size
+	 */
+	private static final int emptyHeaderSize = 21;
+
+	private final String filename;
+	private final boolean compressed = false;
+	private final int originalSize = 0;
+	private final int timestamp = 0;
+	private final int fileSize;
+
+	// non-final; calculated after all headers are created
+	private int fileOffset;
+
+
+	/**
+	 * create a {@link ByteProcessor} for reading header entries
+	 * @return
+	 */
+	public static ByteProcessor<List<Header>> processor() {
+		return new HeadersProcessor();
+	}
+
+
+	/**
+	 * 
+	 * @param filename
+	 * @param dataSize
+	 */
+	Header(final String filename, final int dataSize) {
+		this.filename = filename;
+		this.fileSize = dataSize;
+	}
+
+
+	/**
+	 * the name this file should be written out to
+	 * it may contain subdirectories
+	 */
+	public String getFilename() {
+		return filename;
+	}
+
+
+	/**
+	 * the size of the datablock in bytes
+	 */
+	public int getFileSize() {
+		return fileSize;
+	}
+
+
+	/**
+	 * get the starting point in the pbo for the file
+	 */
+	public int getFileOffset() {
+		return fileOffset;
+	}
+
+
+	/**
+	 * calculate the size of this header
+	 */
+	public int size() {
+		return filename.length() + emptyHeaderSize;
+	}
+
+
+	/**
+	 * write this header to the {@link ByteSink}
+	 * @param byteSink
+	 * @throws Exception
+	 */
+	public void write(final ByteSink byteSink)
+		throws Exception {
+
+		final ByteBuffer buffer = ByteBuffer.allocate(size()).order(ByteOrder.LITTLE_ENDIAN);
+		for (final char ch : filename.toCharArray()) {
+			buffer.put((byte)ch);
+		}
+		buffer.put((byte)0);
+		buffer.putInt(0);
+		buffer.putInt(0);
+		buffer.putInt(0);
+		buffer.putInt(0);
+		buffer.putInt(fileSize);
+		byteSink.write(buffer.array());
+	}
+
+
+	@Override
+	public String toString() {
+		final StringBuilder buffer = new StringBuilder();
+		buffer.append("header{ name:").append(filename).append(" size:").append(fileSize).append(" }");
+		return buffer.toString();
+	}
+
+
+	/**
+	 * process all headers returing a List of Header objects
+	 * @author wassj
+	 */
+	static class HeadersProcessor
+		implements ByteProcessor<List<Header>> {
+
+		private final List<Header> headers = Lists.newArrayList();
+
+
+		public List<Header> getResult() {
+			return headers;
+		}
+
+
+		public boolean processBytes(final byte[] buf, final int off, final int len)
+			throws IOException {
+
+			final ByteBuffer buffer = ByteBuffer.wrap(buf, off, len).order(ByteOrder.LITTLE_ENDIAN);
+			for (Optional<Header> header = read(buffer); header.isPresent(); header = read(buffer)) {
+				headers.add(header.get());
+			}
+
+			// init with the size of all headers
+			int currentDataOffset = sum(headers) + emptyHeaderSize;
+			for (final Header header : headers) {
+				header.fileOffset = currentDataOffset;
+				currentDataOffset += header.getFileSize();
+			}
+			return false;
+		}
+
+
+		Optional<Header> read(final ByteBuffer buffer) {
+			final StringBuilder filename = new StringBuilder();
+			for (byte ch = buffer.get(); ch != 0; ch = buffer.get()) {
+				filename.append((char)ch);
+			}
+
+			final int packaging = buffer.getInt();
+			final int originalSize = buffer.getInt();
+			final int reserved = buffer.getInt();
+			final int timestamp = buffer.getInt();
+			final int datasize = buffer.getInt();
+
+			/*
+			 * the end of the header section is marked with an empty length filename
+			 */
+			if (filename.length() > 0) {
+				return Optional.of(new Header(filename.toString(), datasize));
+			}
+			return Optional.absent();
+		}
+	}
+
+
+	static int sum(final Collection<Header> headers) {
+		int sum = 0;
+		for (final Header header : headers) {
+			sum += header.size();
+		}
+		return sum;
+	}
+}
---
+++ pbos/src/main/java/com/ctc/vht/pbos/Pbos.java
@@ -0,0 +1,103 @@
+package com.ctc.vht.pbos;
+
+import java.io.File;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import com.google.common.hash.Hashing;
+import com.google.common.io.ByteSink;
+import com.google.common.io.ByteSource;
+import com.google.common.io.FileWriteMode;
+import com.google.common.io.Files;
+
+/**
+ * entry point to the PBO API
+ * @author wassj
+ *
+ */
+public class Pbos {
+
+	/**
+	 * unpack the pbo at the specified path to the specified path
+	 * @param source input pbo
+	 * @param target output directory
+	 * @throws Exception
+	 */
+	public static void unpack(final Path source, final Path target)
+		throws Exception {
+
+		Preconditions.checkNotNull(source);
+		Preconditions.checkNotNull(target);
+
+		final File sourceFile = source.toFile();
+		Preconditions.checkArgument(sourceFile.exists());
+
+		final File targetDirectory = target.toFile();
+		Preconditions.checkArgument(!targetDirectory.exists() || targetDirectory.isDirectory(), "target is not a directory; %s", targetDirectory);
+		Preconditions.checkArgument(!targetDirectory.exists() || targetDirectory.listFiles().length == 0, "non-empty target directory; %s", targetDirectory);
+		Preconditions.checkState(targetDirectory.mkdirs(), "could not create directory; %s", targetDirectory);
+
+		for (final Header header : Files.asByteSource(sourceFile).read(Header.processor())) {
+			try (final InputStream is = Files.asByteSource(sourceFile).openBufferedStream()) {
+				final ByteBuffer buffer = ByteBuffer.allocate(header.getFileSize());
+				is.skip(header.getFileOffset());
+				is.read(buffer.array(), 0, header.getFileSize());
+				write(buffer, header.getFilename(), targetDirectory);
+			}
+		}
+	}
+
+
+	public static void pack(final Path source, final Path target)
+		throws Exception {
+
+		Preconditions.checkNotNull(source);
+		Preconditions.checkNotNull(target);
+
+		final File sourceDirectory = source.toFile();
+		Preconditions.checkArgument(sourceDirectory.exists());
+		Preconditions.checkArgument(sourceDirectory.isDirectory());
+
+		final File targetFile = target.toFile();
+		Preconditions.checkArgument(!targetFile.exists(), "target file already exists; %s", targetFile);
+		Preconditions.checkState(targetFile.getParentFile().exists() || targetFile.getParentFile().mkdirs(), "couldnt create target path; %s", targetFile.getParentFile());
+		Preconditions.checkState(targetFile.createNewFile(), "could not create file; %s", targetFile);
+
+		final List<File> files = Lists.newArrayList(sourceDirectory.listFiles());
+		Preconditions.checkArgument(!files.isEmpty(), "directory empty; %s", sourceDirectory);
+		Collections.sort(files);
+
+		final ByteSink byteSink = Files.asByteSink(targetFile, FileWriteMode.APPEND);
+		for (final File file : files) {
+			final ByteSource byteSource = Files.asByteSource(file);
+			new Header(file.getName(), (int)byteSource.size()).write(byteSink);
+		}
+		new Header("", 0).write(byteSink);
+		for (final File file : files) {
+			byteSink.write(Files.asByteSource(file).read());
+		}
+		byteSink.write(ByteBuffer.allocate(21) //
+			.order(ByteOrder.LITTLE_ENDIAN)
+			.put((byte)0)
+			.put(Files.asByteSource(targetFile).hash(Hashing.sha1()).asBytes())
+			.array());
+	}
+
+
+	static void write(final ByteBuffer buffer, final String filename, final File root)
+		throws Exception {
+
+		final File unpacked = new File(root, filename);
+		if (!unpacked.getParentFile().equals(root)) {
+			unpacked.getParentFile().mkdirs();
+		}
+		unpacked.createNewFile();
+		Files.asByteSink(unpacked).write(buffer.array());
+	}
+}
